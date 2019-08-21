01cfed85c637fdf4a6d0c81b852f78604bdd8288
Way <wei.xw.xu@gmail.com>
2019-08-21 12:53:59 -0400

    edited comment for easier understanding

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,7 @@
 export default function palindrome(str) {
 
-  // remove special characters, convert to upper case
+  // remove special characters and spaces, convert to upper case
+  // using regex
   var ustr = str.replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').toUpperCase();
 
   // split each character of the string and save into an array.
